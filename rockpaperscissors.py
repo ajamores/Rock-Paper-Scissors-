c4ebafa2dc660655952b19b0fe77724e50909677
ajamores <aj_amores@hotmail.com>
2023-04-29 20:42:39 -0400

    Update rockpaperscissors.py

--- rockpaperscissors.py
+++ rockpaperscissors.py
@@ -1,55 +1,94 @@
 import random
+flag = True
 
-print("lets play a game!\
- rock, paper, scissors!")
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
 
-status = True
-while True:
-    options = ['rock', 'paper', 'scissors']
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
 
-    computer = random.choice(options)
-    player= None
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
 
-    while player not in options:
-        player = input('rock, paper, or scissors? choice: ').lower()
+while flag:
+    print("\nWelcome! Let's play Rock, Paper, Scissors!")
+    user = int(input("Press 1 for Rock, 2 for Paper, 3 for Scissors: "))
 
-        if player == computer:
-            print(f"computer picked: {computer}")
-            print(f"player picked: {player}")
-            print("Draw!")
 
-        elif player == "rock":
-            if computer == "paper":
-                print(f"computer picked: {computer}")
-                print(f"player picked: {player}")
-                print("Loser!!")
-            if computer == "scissors":
-                print(f"computer picked: {computer}")
-                print(f"player picked: {player}")
-                print("You win!")
 
-        elif player == "paper":
-            if computer == "scissors":
-                print(f"computer picked: {computer}")
-                print(f"player picked: {player}")
-                print("Loser!!")
-            if computer == "rock":
-                print(f"computer picked: {computer}")
-                print(f"player picked: {player}")
-                print("You win!")
+    if user == 1:
+        print(rock)
+        print("You chose Rock!")
+    elif user == 2:
+        print(paper)
+        print("You chose Paper!")
+    elif user == 3:
+        print(scissors)
+        print("You chose Scissors!")
+    else:
+        print("Please input a valid response!")
+        print(user)
 
-        elif player == "scissors":
-            if computer == "rock":
-                print(f"computer picked: {computer}")
-                print(f"player picked: {player}")
-                print("Loser!!")
-            if computer == "paper":
-                print(f"computer picked: {computer}")
-                print(f"player picked: {player}")
-                print("You win!")
+    cpu = random.randint(1, 3)
 
-    again = input("Wanna play again?: [y/n] ").lower()
-#instead of if again == y use if not equal is much more efficient!
-    if again != "y":
-      break
-print("bye loser!")
+    if cpu == 1:
+        print(rock)
+        print("The Computer chose Rock!")
+    elif cpu == 2:
+        print(paper)
+        print("The Computer chose Paper!")
+    elif cpu == 3:
+        print(scissors)
+        print("The Computer chose Scissors!")
+
+    #winning with Rock
+    if user == 1 and cpu == 1:
+        print("\nIt's a Draw!")
+    elif user == 1 and cpu == 2:
+        print("\nYou lose! Loser! Haha!")
+    elif user ==  1 and cpu == 3:
+        print("\nYou Win!")
+
+    #winning with Paper
+
+    if user == 2 and cpu == 1:
+        print("\nYou Win!")
+    elif user == 2 and cpu == 2:
+        print("\nIt's a Draw!")
+    elif user ==  2 and cpu == 3:
+        print("\nYou Lose!")
+
+    #winning with Scissors
+    if user == 3 and cpu == 1:
+        print("\nYou lose! Loser! Haha!")
+    elif user == 3 and cpu == 2:
+        print("\nYou Win!")
+    elif user ==  3 and cpu == 3:
+        print("\nIt's a Draw!")
+
+    again = input("Would you like to play again? (y/n): ").lower()
+
+    if again == "n":
+        print("Thanks for playing!")
+        flag = False
+    elif again != "y":
+        print("Please input a valid response!")
+        again = input("Would you like to play again? (y/n): ").lower()
